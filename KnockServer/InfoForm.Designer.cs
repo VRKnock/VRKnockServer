6f5962876e5520121a8700abcca8f25d8bb0caa3
Th3Shadowbroker <webmaster@m4taiori.de>
2019-12-25 17:16:05 +0100

    Disabled maximize, removed fixed-size and set size-mode to fixed

--- KnockServer/InfoForm.Designer.cs
+++ KnockServer/InfoForm.Designer.cs
@@ -31,8 +31,7 @@ namespace KnockServer
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources =
-                new System.ComponentModel.ComponentResourceManager(typeof(InfoForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfoForm));
             this.label1 = new System.Windows.Forms.Label();
             this.qrPictureBox = new System.Windows.Forms.PictureBox();
             this.hostLabel = new System.Windows.Forms.Label();
@@ -42,65 +41,64 @@ namespace KnockServer
             this.autostartCheckbox = new System.Windows.Forms.CheckBox();
             this.hostTextBox = new System.Windows.Forms.TextBox();
             this.codeTextBox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize) (this.qrPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(446, 18);
+            this.label1.Size = new System.Drawing.Size(382, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Scan the QR Code Below using the VRKnock App!";
             // 
             // qrPictureBox
             // 
-            this.qrPictureBox.Location = new System.Drawing.Point(10, 30);
+            this.qrPictureBox.Location = new System.Drawing.Point(9, 26);
             this.qrPictureBox.Name = "qrPictureBox";
-            this.qrPictureBox.Size = new System.Drawing.Size(199, 200);
+            this.qrPictureBox.Size = new System.Drawing.Size(171, 173);
             this.qrPictureBox.TabIndex = 1;
             this.qrPictureBox.TabStop = false;
             // 
             // hostLabel
             // 
-            this.hostLabel.Location = new System.Drawing.Point(10, 240);
+            this.hostLabel.Location = new System.Drawing.Point(9, 208);
             this.hostLabel.Name = "hostLabel";
-            this.hostLabel.Size = new System.Drawing.Size(50, 20);
+            this.hostLabel.Size = new System.Drawing.Size(43, 17);
             this.hostLabel.TabIndex = 2;
             this.hostLabel.Text = "Host:";
             // 
             // codeLabel
             // 
-            this.codeLabel.Location = new System.Drawing.Point(10, 265);
+            this.codeLabel.Location = new System.Drawing.Point(9, 230);
             this.codeLabel.Name = "codeLabel";
-            this.codeLabel.Size = new System.Drawing.Size(50, 20);
+            this.codeLabel.Size = new System.Drawing.Size(43, 17);
             this.codeLabel.TabIndex = 3;
             this.codeLabel.Text = "Code:";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(10, 400);
+            this.label2.Location = new System.Drawing.Point(9, 347);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(226, 20);
+            this.label2.Size = new System.Drawing.Size(194, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Right-Click the Tray Icon to Exit";
             // 
             // gameActivityCheckbox
             // 
-            this.gameActivityCheckbox.Location = new System.Drawing.Point(16, 300);
+            this.gameActivityCheckbox.Location = new System.Drawing.Point(14, 260);
             this.gameActivityCheckbox.Name = "gameActivityCheckbox";
-            this.gameActivityCheckbox.Size = new System.Drawing.Size(199, 20);
+            this.gameActivityCheckbox.Size = new System.Drawing.Size(171, 17);
             this.gameActivityCheckbox.TabIndex = 5;
             this.gameActivityCheckbox.Text = "Show Current Game In App";
             this.gameActivityCheckbox.UseVisualStyleBackColor = true;
-            this.gameActivityCheckbox.CheckedChanged +=
-                new System.EventHandler(this.gameActivityCheckbox_CheckedChanged);
+            this.gameActivityCheckbox.CheckedChanged += new System.EventHandler(this.gameActivityCheckbox_CheckedChanged);
             // 
             // autostartCheckbox
             // 
-            this.autostartCheckbox.Location = new System.Drawing.Point(16, 330);
+            this.autostartCheckbox.Location = new System.Drawing.Point(14, 286);
             this.autostartCheckbox.Name = "autostartCheckbox";
-            this.autostartCheckbox.Size = new System.Drawing.Size(199, 20);
+            this.autostartCheckbox.Size = new System.Drawing.Size(171, 17);
             this.autostartCheckbox.TabIndex = 6;
             this.autostartCheckbox.Text = "Autostart with SteamVR";
             this.autostartCheckbox.UseVisualStyleBackColor = true;
@@ -108,25 +106,25 @@ namespace KnockServer
             // 
             // hostTextBox
             // 
-            this.hostTextBox.Location = new System.Drawing.Point(66, 237);
+            this.hostTextBox.Location = new System.Drawing.Point(57, 205);
             this.hostTextBox.Name = "hostTextBox";
             this.hostTextBox.ReadOnly = true;
-            this.hostTextBox.Size = new System.Drawing.Size(200, 23);
+            this.hostTextBox.Size = new System.Drawing.Size(172, 20);
             this.hostTextBox.TabIndex = 7;
             // 
             // codeTextBox
             // 
-            this.codeTextBox.Location = new System.Drawing.Point(66, 262);
+            this.codeTextBox.Location = new System.Drawing.Point(57, 227);
             this.codeTextBox.Name = "codeTextBox";
             this.codeTextBox.ReadOnly = true;
-            this.codeTextBox.Size = new System.Drawing.Size(200, 23);
+            this.codeTextBox.Size = new System.Drawing.Size(172, 20);
             this.codeTextBox.TabIndex = 8;
             // 
             // InfoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 511);
+            this.ClientSize = new System.Drawing.Size(269, 443);
             this.Controls.Add(this.codeTextBox);
             this.Controls.Add(this.hostTextBox);
             this.Controls.Add(this.autostartCheckbox);
@@ -136,14 +134,15 @@ namespace KnockServer
             this.Controls.Add(this.hostLabel);
             this.Controls.Add(this.qrPictureBox);
             this.Controls.Add(this.label1);
-            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(330, 550);
-            this.MinimumSize = new System.Drawing.Size(330, 550);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "InfoForm";
             this.Text = "VRKnock Server";
-            ((System.ComponentModel.ISupportInitialize) (this.qrPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
